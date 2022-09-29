466f40cab5cfdc10996100c5b7c31dee9886735e
Gowtham <112604105+Gowtham152003@users.noreply.github.com>
2022-09-29 14:03:53 +0530

    Update Disk scan.cpp
    Enter the max range of disk
    7
    Enter the initial head position
    50
    Enter the size of queue request
    7
    Enter the queue of disk positions to be read
    82
    170
    43
    140
    24
    16
    190
    Disk head moves from 50 to 82 with seek 32
    Disk head moves from 82 to 140 with seek 58
    Disk head moves from 140 to 170 with seek 30
    Disk head moves from 170 to 190 with seek 20
    Disk head moves from 190 to 7 with seek 183
    Disk head moves from 7 to 43 with seek 36
    Disk head moves from 43 to 24 with seek 19
    Disk head moves from 24 to 16 with seek 8
    Disk head moves from 16 to 0 with seek 16
    Total seek time is 402
    Average seek time is 57.428570

--- Disk scan.cpp
+++ Disk scan.cpp
@@ -1,68 +1,125 @@
-#include<stdio.h>
-#include<math.h>
-void scan(int noq, int qu[10], int st, int ch) 
-{
-int i,j,s=0; 
-for(i=0;i<noq;i++) 
-{
-if(st < qu[i])
-{ 
-for(j=i-1; j>= 0;j--) 
- { 
- s=s+abs(st - qu[j]); 
- st = qu[j]; 
- } 
-if(ch == 3) 
- { 
- s = s + abs(st - 0); 
- st = 0; 
- } 
-for(j = 1;j < noq;j++) 
- { 
- s= s + abs(st - qu[j]); 
- st = qu[j]; 
- } 
- break; 
- } }
-printf("\n Total seek time : %d",s); 
-} 
+#include <stdio.h>
+#include <math.h>
 int main()
- { 
- int n,qu[20],st,i,j,t,noq,ch,visit[20]; 
-printf("\n Enter the maximum number of cylinders : "); 
-scanf("%d",&n); 
-printf("enter number of queue elements"); 
-scanf("%d",&noq); 
-printf("\n Enter the work queue"); 
-for(i=0;i<noq;i++) 
- { 
-scanf("%d",&qu[i]); 
- visit[i] = 0; 
- }
-printf("\n Enter the disk head starting posision: \n"); 
-scanf("%d",&st); 
-while(1) 
 {
-printf("\n\n\t\t MENU \n"); 
-printf("\n\n\t\t 1. SCAN \n"); 
-printf("\n\n\t\t 2. EXIT \n"); 
-printf("\nEnter your choice: "); 
-scanf("%d",&ch);
-if(ch > 2)
-{ 
-for(i=0;i<noq;i++) 
-for(j=i+1;j<noq;j++) 
- if(qu[i]>qu[j]) 
- { 
- t=qu[i]; 
-qu[i] = qu[j]; 
- qu[j] = t; 
- } } 
- switch(ch) 
- {
-case 1: printf("\n SCAN \n"); 
- printf("\n*****\n"); 
-scan(noq,qu,st,ch); 
- break; 
-case 2: exit(0); 
-} } }
+
+    int queue[20], n, head, i, j, k, seek = 0, max, diff, temp, queue1[20], 
+    queue2[20], temp1 = 0, temp2 = 0;
+
+    float avg;
+
+    printf("Enter the max range of disk\n");
+
+    scanf("%d", &max);
+
+    printf("Enter the initial head position\n");
+
+    scanf("%d", &head);
+
+    printf("Enter the size of queue request\n");
+
+    scanf("%d", &n);
+
+    printf("Enter the queue of disk positions to be read\n");
+
+    for (i = 1; i <= n; i++)
+
+    {
+
+        scanf("%d", &temp);
+
+        if (temp >= head)
+
+        {
+
+            queue1[temp1] = temp;
+
+            temp1++;
+        }
+
+        else
+
+        {
+
+            queue2[temp2] = temp;
+
+            temp2++;
+        }
+    }
+
+    for (i = 0; i < temp1 - 1; i++)
+
+    {
+
+        for (j = i + 1; j < temp1; j++)
+
+        {
+
+            if (queue1[i] > queue1[j])
+
+            {
+
+                temp = queue1[i];
+
+                queue1[i] = queue1[j];
+
+                queue1[j] = temp;
+            }
+        }
+    }
+
+    for (i = 0; i < temp2 - 1; i++)
+
+    {
+
+        for (j = i + 1; j < temp2; j++)
+
+        {
+
+            if (queue2[i] < queue2[j])
+
+            {
+
+                temp = queue2[i];
+
+                queue2[i] = queue2[j];
+
+                queue2[j] = temp;
+            }
+        }
+    }
+
+    for (i = 1, j = 0; j < temp1; i++, j++)
+
+        queue[i] = queue1[j];
+
+    queue[i] = max;
+
+    for (i = temp1 + 2, j = 0; j < temp2; i++, j++)
+
+        queue[i] = queue2[j];
+
+    queue[i] = 0;
+
+    queue[0] = head;
+
+    for (j = 0; j <= n + 1; j++)
+
+    {
+
+        diff = abs(queue[j + 1] - queue[j]);
+
+        seek += diff;
+
+        printf("Disk head moves from %d to %d with seek %d\n", queue[j], 
+        queue[j + 1], diff);
+    }
+
+    printf("Total seek time is %d\n", seek);
+
+    avg = seek / (float)n;
+
+    printf("Average seek time is %f\n", avg);
+
+    return 0;
+}
